d992f0ff507153640cd995e23c08f0bde78138ff
LucasSilverio <lucassilverioesantos@gmail.com>
2024-01-12 10:35:47 -0300

    design ajusts

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/logo preta.png")
+    img.setAttribute("src", "./assets/logo-light.png")
   } else {
-    img.setAttribute("src", "./assets/logo branca.png")
+    img.setAttribute("src", "./assets/logo-dark.png")
   }
 }
